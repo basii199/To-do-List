68f07895c3a24de2e8314af9c8864edf48dc3650
basii199 <ubokabasi135@gmail.com>
2024-11-01 22:23:13 +0100

    Make New task button interactive

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
-document.querySelector('.new-to-do-button').addEventListener('click', ()=>{
+const addTodo = document.querySelector('.new-to-do-button')
+addTodo.addEventListener('click', ()=>{    
+    document.querySelector('.container').classList.remove('no-display')
     document.querySelector('.input-element').focus()
 })
+ 
