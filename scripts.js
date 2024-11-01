b85ddae4bb433f15e6dbb2046d81299ab8b7f731
basii199 <ubokabasi135@gmail.com>
2024-11-02 00:41:49 +0100

    Visual adjustments

--- scripts.js
+++ scripts.js
@@ -3,4 +3,41 @@ addTodo.addEventListener('click', ()=>{
     document.querySelector('.container').classList.remove('no-display')
     document.querySelector('.input-element').focus()
 })
- 
+
+let toDoArray = [
+    /* {id: '', task: 'task 1'},
+    {id: '', task: 'task 2'},
+    {id: '', task: 'task 3'},
+    {id: '', task: 'task 4'} */
+]
+
+
+
+renderTasks()
+function renderTasks(){
+    let HTML = ''
+    let checked = ''
+
+    toDoArray.forEach((toDoItem, i)=>{
+        toDoItem.id = i
+        let html = `
+            <div class="to-do-items">
+                    <input type="checkbox" class="checkbox" ${checked}>
+                    ${toDoItem.task}
+                </div>
+        `
+        HTML += html
+})
+    document.querySelector('.main-body').innerHTML = HTML
+}
+
+
+const addTodoSecondary = document.querySelector('.add-button')
+addTodoSecondary.addEventListener('click', ()=>{ 
+    let todoItem = document.querySelector('.input-element') 
+    toDoArray.push({id: '', task: todoItem.value})
+    console.log(toDoArray)
+    todoItem.value = ''
+    renderTasks()
+    document.querySelector('.container').classList.add('no-display')
+})
